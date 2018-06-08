fb0db160ce309ccc178bec5d5f62118ea8daf1df
weibo <287595851@qq.com>
2018-06-08 13:56:38 +0800

    update mongoose

--- config/config.default.js
+++ config/config.default.js
@@ -10,10 +10,22 @@
 exports.mongoosee = {
   mongodb: {
     url: 'mongodb://localhost:27017',
-    user: "test",
-    password: "test",
-    dbName: "test",
-    options: {},
+    options: {
+      auth: {
+        user: "test",
+        password: "test",
+        dbName: "test",
+      },
+      poolSize: 20,
+      ssl: false,
+      autoReconnect: true,
+      noDelay: true,
+      keepAlive: 30000,
+      connectTimeoutMS: 30000,
+      socketTimeoutMS: 360000,
+      reconnectTries: 30,
+      reconnectInterval: 1000,
+    },
   },
   egg: {
     modelDir: 'app/model', // specify model dir path
